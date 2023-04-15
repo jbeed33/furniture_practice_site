d941f0f6c637cca248be897b29edbb78d2400f4c
Joshua Beed <jbeed33@gmail.com>
2023-04-15 10:00:49 -0700

    updated mobile menu functionallity

--- js/mobile_home_script.js
+++ js/mobile_home_script.js
@@ -1,1 +1,15 @@
 let mobile_menu = document.querySelector('#mobile_menu');
+let mobile_menu_nav_bar = document.querySelector('#nav-bar-container');
+let mobile_menu_close = document.querySelector('#close_mobile_menu_btn');
+
+mobile_menu.addEventListener('click', () =>{
+    if(mobile_menu_nav_bar.classList.contains('hidden') ){
+        mobile_menu_nav_bar.classList.toggle('hidden');
+    }
+});
+
+mobile_menu_close.addEventListener('click', () =>{
+    if(!mobile_menu_nav_bar.classList.contains('hidden') ){
+        mobile_menu_nav_bar.classList.toggle('hidden');
+    }
+})
