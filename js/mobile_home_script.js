5a9f4ba4303c97e173e67d9ec8f8906138d83b62
Joshua Beed <jbeed33@gmail.com>
2023-04-18 11:45:09 -0700

    added animations to mobile layout

--- js/mobile_home_script.js
+++ js/mobile_home_script.js
@@ -2,7 +2,30 @@ let mobile_menu = document.querySelector('#mobile_menu');
 let mobile_menu_nav_bar = document.querySelector('#nav-bar-container');
 let mobile_menu_close = document.querySelector('#close_mobile_menu_btn');
 
+const animatedOnScrollElements = document.querySelectorAll('.hidden');
+
+// const observer = new IntersectionObserver((entries) =>{
+//     entries.forEach((entry) =>{
+//         entry.target.classList.remove('.hidden', entry.isIntersecting);
+//     })
+// },
+// {
+//     threshold: 1
+// } 
+// );
+
+// animatedOnScrollElements.forEach(el =>{
+//     observer.observe(el);
+// })
+
 mobile_menu.addEventListener('click', () =>{
+
+    // only used in the intial loading state
+    if(mobile_menu_nav_bar.classList.contains('initial') ){
+        mobile_menu_nav_bar.classList.remove('initial');
+        mobile_menu_nav_bar.classList.toggle('hidden');
+    }
+
     if(mobile_menu_nav_bar.classList.contains('hidden') ){
         mobile_menu_nav_bar.classList.toggle('hidden');
     }
